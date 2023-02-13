844e32500cc2293180ac91777daa8c40323e1e9c
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2023-02-12 21:08:18 -0500

    Week2

---
+++ BryanEuroCuisine.sql
@@ -0,0 +1,19 @@
+create database bryanEuroCuisine;
+
+use bryanEuroCuisine;
+#Created table and columns for Signup
+-- ALTER TABLE `bryaneurocuisine`.`signup` 
+-- ADD COLUMN `Username` INT(20) Primary Key NOT NULL,
+-- ADD COLUMN `Password` VARCHAR(11) NOT NULL AFTER `Username`,  
+-- ADD COLUMN `Firstname` VARCHAR(45) NULL AFTER `Password`,
+-- ADD COLUMN `Lastname` VARCHAR(45) NULL AFTER `Firstname`,
+-- ADD COLUMN `Email` VARCHAR(45) NULL AFTER `Lastname`,
+-- ADD COLUMN `Birthdate` DATE NULL AFTER `Email`,
+-- ADD COLUMN `Phone` VARCHAR(10) NULL AFTER `Birthdate`,
+-- ADD UNIQUE INDEX `_UNIQUE` (`Password` ASC) VISIBLE,
+-- ADD UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE,
+-- ADD UNIQUE INDEX `Phone_UNIQUE` (`Phone` ASC) VISIBLE,
+-- DROP INDEX `Username_UNIQUE`
+
+
+
